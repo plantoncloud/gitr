486e710228ef6cd1ec4a1a6f2c9ccc2b7393b382
swarupdonepudi <swarup.donepudi@gmail.com>
2021-04-26 20:41:23 -0700

    removed long

--- cmd/commands.go
+++ cmd/commands.go
@@ -9,27 +9,23 @@ var h = &cmdHandler{}
 var branches = &cobra.Command{
 	Use:   "branches",
 	Short: "open branches on scm web interface",
-	Long:  ``,
 	Run:   h.remoteRepoHandler(h.branches),
 }
 
 var commits = &cobra.Command{
 	Use:   "commits",
 	Short: "open commits on scm web interface",
-	Long:  ``,
 	Run:   h.remoteRepoHandler(h.commits),
 }
 
 var issues = &cobra.Command{
 	Use:   "issues",
 	Short: "open issues on scm web interface",
-	Long:  ``,
 	Run:   h.remoteRepoHandler(h.issues),
 }
 var pipelines = &cobra.Command{
 	Use:     "pipelines",
 	Short:   "open pipelines on scm web interface",
-	Long:    ``,
 	Aliases: []string{"pipe"},
 	Run:     h.remoteRepoHandler(h.pipelines),
 }
@@ -37,28 +33,24 @@ var pipelines = &cobra.Command{
 var prs = &cobra.Command{
 	Use:   "prs",
 	Short: "open pull requests on scm web interface",
-	Long:  ``,
 	Run:   h.remoteRepoHandler(h.prs),
 }
 
 var releases = &cobra.Command{
 	Use:   "releases",
 	Short: "open releases on scm web interface",
-	Long:  ``,
 	Run:   h.remoteRepoHandler(h.releases),
 }
 
 var rem = &cobra.Command{
 	Use:   "rem",
 	Short: "opens the repo on the scm web interface",
-	Long:  ``,
 	Run:   h.remoteRepoHandler(h.rem),
 }
 
 var tags = &cobra.Command{
 	Use:   "tags",
 	Short: "open tags on scm web interface",
-	Long:  ``,
 	Run:   h.remoteRepoHandler(h.tags),
 }
 
