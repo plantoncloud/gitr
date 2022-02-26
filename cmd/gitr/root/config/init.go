b42e55cf677845a2c4dad1a3c4cdf233b78753ba
swarupdonepudi <swarup.donepudi@gmail.com>
2022-02-25 17:37:46 -0800

    removed internal config

--- cmd/gitr/root/config/init.go
+++ cmd/gitr/root/config/init.go
@@ -3,7 +3,7 @@ package config
 import (
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
-	"github.com/swarupdonepudi/gitr/v2/internal/config"
+	"github.com/swarupdonepudi/gitr/v2/pkg/config"
 )
 
 var Init = &cobra.Command{
--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"fmt"
+	"github.com/leftbin/go-util/pkg/file"
 	"github.com/pkg/errors"
 	"gopkg.in/yaml.v3"
 	"os"
@@ -19,6 +21,25 @@ const (
 	Http                HttpScheme  = "http"
 	Https               HttpScheme  = "https"
 )
+
+func EnsureInitialConfig() error {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return errors.Wrap(err, "failed to get user home dir")
+	}
+	gitrConfigFile := fmt.Sprintf("%s/.gitr.yaml", homeDir)
+	cfg := NewDefaultConfig()
+	d, err := yaml.Marshal(&cfg)
+	if err != nil {
+		return errors.Wrap(err, "failed to marshal config")
+	}
+	if !file.IsFileExists(gitrConfigFile) {
+		if err = os.WriteFile(gitrConfigFile, d, 0644); err != nil {
+			return errors.Wrapf(err, "failed to write file %s", gitrConfigFile)
+		}
+	}
+	return nil
+}
 
 func GetScmHost(cfg *GitrConfig, hostname string) (*ScmHost, error) {
 	//return the scm system from config file
@@ -45,4 +66,27 @@ func NewGitrConfig() (*GitrConfig, error) {
 		return nil, errors.Wrapf(err, "failed to unmarshal %s file", gitrConfigYaml)
 	}
 	return &cfg, nil
+}
+
+func NewDefaultConfig() *GitrConfig {
+	return &GitrConfig{
+		CopyRepoPathCdCmdToClipboard: false,
+		Scm: &Scm{
+			HomeDir: "",
+			Hosts:   defaultScmSystems(),
+		},
+	}
+}
+
+func defaultScmSystems() []*ScmHost {
+	defaultCloneConfig := &CloneConfig{
+		HomeDir:              "",
+		AlwaysCreDir:         true,
+		IncludeHostForCreDir: true,
+	}
+	return []*ScmHost{
+		{Scheme: Https, Hostname: "github.com", Provider: GitHub, DefaultBranch: "master", Clone: defaultCloneConfig},
+		{Scheme: Https, Hostname: "gitlab.com", Provider: GitLab, DefaultBranch: "main", Clone: defaultCloneConfig},
+		{Scheme: Https, Hostname: "bitbucket.org", Provider: BitBucketCloud, DefaultBranch: "master", Clone: defaultCloneConfig},
+	}
 }
